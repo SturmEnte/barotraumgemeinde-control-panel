ed8db639ea54c8cdf15f4bc4efdf37f1df1c6ec1
Jonas <jonas@sturmente.de>
2025-05-10 21:18:18 +0200

    feat: Display restart initiation time on dashboard

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,12 +1,42 @@
 import { Router, urlencoded } from "express";
 import { join } from "path";
-import { writeFileSync } from "fs";
+import { readFileSync, writeFileSync, existsSync } from "fs";
+
+import formatDate from "../util/formatDate";
 
 const config = require("../../config.json");
 
 const router = Router();
 
 router.get("/", (req, res) => {
+	// Add restart inititiation times if possible
+	let servers = config.servers;
+
+	for (let i in servers) {
+		let server = servers[i];
+
+		const filePath = join(__dirname, "../../", server.path);
+
+		if (!existsSync(filePath)) {
+			continue;
+		}
+
+		let fileContent = readFileSync(filePath).toString();
+
+		if (!fileContent) {
+			continue;
+		}
+
+		let date = new Date(Number(fileContent));
+
+		// Check if the date object is valid to prevent errors showing on the website due to invalid timestamps
+		if (isNaN(date.getTime())) {
+			continue;
+		}
+
+		server.restartInitiatedAt = formatDate(date);
+	}
+
 	res.render(join(__dirname, "../views/dashboard.ejs"), { servers: config.servers, status: req.query.status, result: req.query.result });
 });
 
@@ -33,7 +63,7 @@ router.post("/restart", (req, res) => {
 
 			try {
 				writeFileSync(filePath, Date.now().toString());
-				console.log("Initiated restart at " + Date.now());
+				console.log("Initiated restart at " + formatDate(new Date()));
 				result = "Initiated restart";
 				status = "1";
 			} catch (err) {
